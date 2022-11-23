bea70365e612fe95059ad4ef3e12febfcb1ad32b
Rohit Waghchaure <rohitw1991@gmail.com>
2022-11-23 14:23:21 +0530

    feat: on cancel of issue cancel/delete service record

---
+++ selco/selco/custom_service_record.py
@@ -0,0 +1,20 @@
+import frappe
+
+def on_cancel_event(doc, method=None):
+	update_issue_status(doc)
+
+def update_issue_status(doc):
+	if doc.selco_complaint_number:
+		workflow_state = frappe.get_cached_value("Issue",
+			doc.selco_complaint_number, "workflow_state")
+
+		if workflow_state in ["Complaint Closed By CSD",
+			"Complaint Closed By Branch", "Complaint Attended By CSE - Still Open"]:
+			frappe.db.set_value("Issue", doc.selco_complaint_number,
+				"workflow_state", "Complaint Assigned to CSE")
+
+			frappe.db.sql("""
+				DELETE `tabService Record Details Issue`
+					where service_record_no = %s
+			""", doc.name)
+
